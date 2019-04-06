435f0bb5f799b64eb0472290eea3bf6300cb1908
Antony Nyagah <antonynyagah@antonys-MBP.attlocal.net>
2019-04-05 23:23:12 -0400

    most of mvp done

--- friends/src/component/friendslist.js
+++ friends/src/component/friendslist.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-const FriendsList = (props) => {
-    return ( 
-        <div className='friend-card'>
-            <p>{props.friend.name}</p>
-            <p>{props.friend.age}</p>
-            <p>{props.friend.email}</p>
+const FriendList = props => {
+
+    function routeToItem(ev, item) {
+        ev.preventDefault();
+        props.history.push(`/${item.id}`);
+        props.getItemById(item.id)
+      }
+
+    return (
+
+        <div>
+        {props.friends.map(friend => 
+            // <Link to={`/${friend.id}`}><div>{friend.name} {friend.age} {friend.email}</div></Link>
+            <div
+            onClick={ev => routeToItem(ev, friend)}
+          className="item-card"
+          key={friend.id}>{friend.name} {friend.age} {friend.email}</div>
+            )
+        }  
         </div>
-     );
+    )
+
 }
 
-export default FriendsList;
+export default FriendList
